487ed32d8089bb8f88b34a3edbedcc14948f443b
mmachenry <mike.machenry@gmail.com>
2026-01-29 12:35:25 -0500

    days off target and clean up code

--- src/Schedule.hs
+++ src/Schedule.hs
@@ -3,7 +3,7 @@ module Schedule (main, basicPrintModel) where
 import Data.Time.Calendar
 import Data.Time.Calendar.Month
 import Control.Monad (forM_)
-import Data.SBV hiding (listArray)
+import Data.SBV hiding (listArray, fromEnum)
 import Data.Array
 import Text.Printf
 
@@ -35,59 +35,56 @@ main = do
           Nothing -> putStrLn $ "<no deviation>"
     _ -> putStrLn "Not satisfiable"
 
-
 schedule :: [Month] -> IO OptimizeResult
 schedule cohorts = optimize Lexicographic $ do
   allDays <- mapM buildCohortEvents cohorts
-  constrain $ distinct (concat allDays)
+  constrain $ distinct (concatMap elems allDays)
 
   forM_ (zip cohorts allDays) $ \(cohort, cohortDays)->
     forM_ [minBound .. maxBound] $ \visit-> do
       minimize
         ((varName cohort visit) ++ "_deviation")
-        (visitDeviation cohort visit (listArray (minBound,maxBound) cohortDays))
+        (visitDeviation cohort visit cohortDays)
 
-
-buildCohortEvents :: Month -> Symbolic [SInteger]
+buildCohortEvents :: Month -> Symbolic (Array Visit SInteger)
 buildCohortEvents cohort = do
-  vars <- mapM (sInteger . (varName cohort)) [minBound::Visit .. maxBound]
-  let days = listArray (minBound,maxBound) vars
+  days <- genArrayM (minBound, maxBound) (sInteger . varName cohort)
 
   -- All visits must be on the same day of the week
-  constrain $ allEqual (map (flip sMod 7) vars)
+  constrain $ allEqual (map (`sMod` 7) (elems days))
   -- Visits must be on one of the available visit days
   constrain $ isAvailableWeekDay (days!W16)
 
-  -- The first two visits can be 2 weeks odd of perfect
+  -- The first two visits can be 2 weeks odd of target
   constrain $ visitDeviation cohort W16 days .<= 2 * 7
   constrain $ visitDeviation cohort W24 days .<= 2 * 7
 
-  -- The W28 visit can be one week off perfect and all
+  -- The W28 visit can be one week off target and all
   -- of the two-week visits must be two weeks apart 
   -- exactly.
   constrain $ visitDeviation cohort W28 days .<= 1 * 7
-  constrain $ nWeeksApart 2 (days!W28) (days!W30)
-  constrain $ nWeeksApart 2 (days!W30) (days!W32)
-  constrain $ nWeeksApart 2 (days!W32) (days!W34)
+  constrain $ nWeeksApart 2 W28 W30 days
+  constrain $ nWeeksApart 2 W30 W32 days
+  constrain $ nWeeksApart 2 W32 W34 days
 
-  -- The W37 visit can be one week off perfect and all
+  -- The W37 visit can be one week off target and all
   -- of the one-week visits must be one week apart
   -- exactly.
   constrain $ visitDeviation cohort W37 days .<= 1 * 7
-  constrain $ nWeeksApart 1 (days!W37) (days!W38)
-  constrain $ nWeeksApart 1 (days!W38) (days!W39)
+  constrain $ nWeeksApart 1 W37 W38 days
+  constrain $ nWeeksApart 1 W38 W39 days
 
   -- The final 6 week post partum visit can be two
-  -- weeks off of perfect
+  -- weeks off of target
   constrain $ visitDeviation cohort W46 days .<= 2 * 7
 
-  pure vars
+  pure days
 
 varName :: Month -> Visit -> String
 varName (YearMonth year month) visit = printf "%04d-%02d-%s" year month (show visit)
 
-nWeeksApart :: SInteger -> SInteger -> SInteger -> SBool
-nWeeksApart n d1 d2 = d1 + (n * 7) .== d2
+nWeeksApart :: SInteger -> Visit -> Visit -> Array Visit SInteger -> SBool
+nWeeksApart n v1 v2 days = days!v1 + (n * 7) .== days!v2
 
 visitDeviation :: Month -> Visit -> Array Visit SInteger -> SInteger
 visitDeviation cohort visit days =
@@ -95,10 +92,7 @@ visitDeviation cohort visit days =
   in abs (optimal - days!visit)
 
 isAvailableWeekDay :: SInteger -> SBool
-isAvailableWeekDay d =
-     d `sMod` 7 .== 2   -- Friday
- .|| d `sMod` 7 .== 6   -- Tuesday
- .|| d `sMod` 7 .== 5   -- Monday
+isAvailableWeekDay d = sElem (d `sMod` 7) [2,5,6] -- Monday, Tuesday, Friday
 
 dueDateTolastMenstrualPeriod :: Day -> Day
 dueDateTolastMenstrualPeriod = addDays (-40 * 7)
@@ -108,8 +102,13 @@ cohortDueDate (YearMonth year month) = YearMonthDay year month 15
 
 targetDay :: Month -> Visit -> Day
 targetDay cohort visit =
-    addDays (7 * numWeeks!visit)
-            (dueDateTolastMenstrualPeriod (cohortDueDate cohort))
+  addDays (7 * numWeeks!visit)
+          (dueDateTolastMenstrualPeriod (cohortDueDate cohort))
   where numWeeks =
           listArray (minBound, maxBound) [16, 24, 28, 30, 32, 34, 37, 38, 39, 46]
 
+genArrayM
+  :: (Ix i, Monad m)
+  => (i,i) -> (i -> m e) -> m (Array i e)
+genArrayM b f =
+  listArray b <$> mapM f (range b)
